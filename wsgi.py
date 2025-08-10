f1a27d070a1dd26e91c5844b9f4681166e3df532
Manoel Jose Feitosa <manoelbd2012@gmail.com>
2025-08-10 20:17:36 -0300

    alteração dos arquivos setting-asgi-wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,5 @@
-"""
-WSGI config for granprix project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-# CORRIGIDO: Aponta para o arquivo 'settings.py' na raiz do projeto.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-
 application = get_wsgi_application()
