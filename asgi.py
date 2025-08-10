25c4d2eda726e27fbfbbbe1da0521fc562c23197
Manoel Jose Feitosa <manoelbd2012@gmail.com>
2025-08-10 20:13:02 -0300

    Correção de estrutura de pastas

--- asgi.py
+++ asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for competicao project.
+ASGI config for granprix project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'competicao.settings')
+# CORRIGIDO: Aponta para o arquivo 'settings.py' na raiz do projeto.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_asgi_application()
